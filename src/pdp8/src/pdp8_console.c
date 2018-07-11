65c3b45965fb1f920d8d77b8d025630a3af6f9a5
Jim Geist <jimge@rodentia.net>
2018-07-11 16:31:54 -0700

    TT output

---
+++ src/pdp8/src/pdp8_console.c
@@ -0,0 +1,203 @@
+/**
+ * IOT dispatch for KL8-E: TTY/LC8-E: Decwriter interface and PC8-E: reader/punch 
+ */
+#include <stdlib.h>
+ 
+#include "pdp8/devices.h"
+
+
+
+static int tty_install(pdp8_device_t *dev, pdp8_t *pdp8);
+static void tty_reset(pdp8_device_t *dev);
+static void tty_dispatch(pdp8_device_t *dev, pdp8_t *pdp8, uint12_t opword);
+static void tty_free(pdp8_device_t *dev);
+
+static void fire_interrupts(pdp8_console_t *con);
+
+struct pdp8_console_t {
+    pdp8_device_t device;
+    pdp8_t *pdp8;
+    uint32_t kbd_intr_bit;
+    uint32_t prt_intr_bit;
+    uint32_t rdr_intr_bit;
+    uint32_t pun_intr_bit;
+    uint32_t all_bits;
+
+    uint32_t dev_flags;
+    uint32_t enable_flags;
+
+    uint8_t kbd_buffer;
+    uint8_t pun_buffer;
+
+    pdp8_console_callbacks_t callbacks;
+};
+
+int pdp8_install_console(pdp8_t *pdp8, pdp8_console_callbacks_t *callbacks, pdp8_console_t **console) {
+    pdp8_console_t *dev = calloc(1, sizeof(pdp8_console_t));
+    if (!dev) {
+        return PDP8_ERR_MEMORY;
+    }
+
+    int bit = pdp8_alloc_intr_bits(pdp8, 4);
+    if (bit < 0) {
+        free(dev);
+        return bit;
+    }
+
+    dev->pdp8 = pdp8;
+
+    dev->device.install = &tty_install;
+    dev->device.reset = &tty_reset;
+    dev->device.dispatch = &tty_dispatch;
+    dev->device.free = &tty_free;
+
+    dev->kbd_intr_bit = 1 << bit++;
+    dev->prt_intr_bit = 1 << bit++;
+    dev->rdr_intr_bit = 1 << bit++;
+    dev->pun_intr_bit = 1 << bit++;
+    dev->all_bits = 
+        dev->kbd_intr_bit |
+        dev->prt_intr_bit |
+        dev->rdr_intr_bit |
+        dev->pun_intr_bit;
+
+    dev->dev_flags = 0;
+    dev->enable_flags = dev->all_bits;
+
+    dev->callbacks = *callbacks;
+    
+    int ret = pdp8_install_device(pdp8, &dev->device);
+    if (ret < 0) {
+        free(dev);
+        return ret;
+    }
+
+    *console = dev;
+    return 0;
+}
+
+static int tty_install(pdp8_device_t *dev, pdp8_t *pdp8) {
+    for (int id = 001; id <= 004; id++) {
+        pdp8->device_handlers[id] = dev;
+    }
+
+    return 0;
+}
+
+
+int pdp8_console_kbd_byte(pdp8_console_t *con, uint8_t ch) {
+    if (con->dev_flags & con->kbd_intr_bit) {
+        return PDP8_ERR_BUSY;
+    }
+
+    /* NB TTY and original DECwriter set the high bit on input data */
+    con->kbd_buffer = ch | 0200;
+
+    con->dev_flags |= con->kbd_intr_bit;
+    fire_interrupts(con);
+
+    return 0;
+}
+
+int pdp8_console_rdr_byte(pdp8_console_t *dev, uint8_t ch) {
+    return 0;
+}
+
+static void tty_reset(pdp8_device_t *dev) {
+    pdp8_console_t *con = (pdp8_console_t *)dev;
+    con->dev_flags = 0;
+    con->enable_flags = con->all_bits;
+    fire_interrupts(con);
+}
+
+static void tty_free(pdp8_device_t *dev) {
+    free(dev);
+}
+
+static void tty_dispatch(pdp8_device_t *dev, pdp8_t *pdp8, uint12_t opword) {
+    pdp8_console_t *con = (pdp8_console_t *)dev;
+    switch (opword) {
+        case KCF:
+            con->dev_flags &= ~con->kbd_intr_bit;
+            fire_interrupts(con);
+            con->callbacks.kbd_ready(con->callbacks.ctx);
+            break;
+
+        case KSF: 
+            if (con->dev_flags & con->kbd_intr_bit) {
+                pdp8->pc = INC12(pdp8->pc);
+            }
+            break;
+
+        case KCC:
+            con->dev_flags &= ~con->kbd_intr_bit;
+            fire_interrupts(con);
+            con->pdp8->ac = 0;
+            con->callbacks.kbd_ready(con->callbacks.ctx);
+            break;
+        
+        case KRS:
+            con->pdp8->ac |= con->kbd_buffer;
+            break;
+
+        case KIE:
+            if (con->pdp8->ac & BIT11) {
+                con->enable_flags |= con->kbd_intr_bit;
+            } else {
+                con->enable_flags &= ~con->kbd_intr_bit;
+            }
+            fire_interrupts(con);
+            break;
+
+        case KRB:
+            con->dev_flags &= ~con->kbd_intr_bit;
+            fire_interrupts(con);
+            con->pdp8->ac = con->kbd_buffer;
+            con->callbacks.kbd_ready(con->callbacks.ctx);
+            break;
+
+        case TFL:
+            con->dev_flags |= con->prt_intr_bit;
+            fire_interrupts(con);
+            break;
+
+        case TSF:
+            if (con->dev_flags & con->prt_intr_bit) {
+                pdp8->pc = INC12(pdp8->pc);
+            }
+            break;
+
+        case TCF:
+            con->dev_flags &= ~con->prt_intr_bit;
+            fire_interrupts(con);
+            break;
+
+        case TPC:
+            con->callbacks.print(con->callbacks.ctx, con->pdp8->ac & 0177);
+            con->dev_flags |= con->prt_intr_bit;
+            fire_interrupts(con);
+            break;
+
+        case TSK:
+            if (con->pdp8->intr_mask & (con->prt_intr_bit | con->kbd_intr_bit)) {
+                pdp8->pc = INC12(pdp8->pc);
+            }
+            return;
+        
+        case TLS:
+            /* TODO this is TCF + TPC, but we immediately turn around and set
+             * the done flag again. we should introduce some real-world
+             * delay here.
+             */
+            con->callbacks.print(con->callbacks.ctx, con->pdp8->ac & 0177);
+            con->dev_flags |= con->prt_intr_bit;
+            fire_interrupts(con);
+            break;
+    }
+}
+
+void fire_interrupts(pdp8_console_t *con) {
+    /* set interrupt bits if device flag is set and enable is on */
+    uint32_t mask = con->dev_flags & con->enable_flags;
+    con->pdp8->intr_mask = (con->pdp8->intr_mask & ~con->all_bits) | mask;
+}
---
+++ src/pdp8/tests/test_console.c
@@ -0,0 +1,332 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "tests.h"
+#include "pdp8/devices.h"
+
+typedef struct callback_mock_t {
+    int kbd_ready;
+    char printed;
+} callback_mock_t;
+
+static void tty_free(void *ctx) {
+
+}
+
+static void tty_kbd_ready(void *ctx) {
+    ((callback_mock_t *)ctx)->kbd_ready++;
+}
+
+static void tty_print(void *ctx, char ch) {
+    ((callback_mock_t *)ctx)->printed = ch;
+}
+
+static void tty_rdr_ready(void *ctx) {
+
+}
+
+static void tty_punch(void *ctx, char ch) {
+
+}
+
+static int setup(callback_mock_t *mocks, pdp8_t **pdp8, pdp8_console_t **con) {
+    *pdp8 = pdp8_create();
+
+    *con = NULL;
+
+    memset(mocks, 0, sizeof(*mocks));
+
+    pdp8_console_callbacks_t callbacks;
+    callbacks.ctx = mocks;
+    callbacks.free = &tty_free;
+    callbacks.kbd_ready = &tty_kbd_ready;
+    callbacks.print = &tty_print;
+    callbacks.rdr_ready = &tty_rdr_ready;
+    callbacks.punch = &tty_punch;
+
+    int ret = pdp8_install_console(*pdp8, &callbacks, con);
+    if (ret < 0) {
+        pdp8_free(*pdp8);
+    }
+
+    return ret;
+}
+
+DECLARE_TEST(con_KSF_KRB, "KSF and KRB instructions") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8_console_kbd_byte(con, '@');
+
+    pdp8->core[01000] = PDP8_ION;
+    pdp8->core[01001] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0001);
+    /* we should interrupt at the start of this instruction */
+    pdp8->core[01002] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0002);
+
+    pdp8->core[1] = KSF;
+    pdp8->core[2] = PDP8_OPR_GRP2 | PDP8_OPR_GRP2_HLT;
+    pdp8->core[3] = KRB;
+    pdp8->core[4] = KSF;
+    
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* SKON */
+    pdp8_step(pdp8);        /* first JMP */
+    pdp8_step(pdp8);        /* takes interrupt, executes KSF */
+
+    ASSERT_V(callback_mocks.kbd_ready == 0, "kbd_ready not yet called");
+    pdp8_step(pdp8);        /* KRB (HLT should be skipped) */
+    ASSERT_V(callback_mocks.kbd_ready == 1, "kbd_ready called");
+        
+    ASSERT_V(pdp8->pc == 4, "Interrupt routine executed");
+    ASSERT_V(pdp8->ac == (0200 | '@'), "Keyboard data received"); 
+
+    pdp8_step(pdp8);        /* KSF (should not skip) */
+    ASSERT_V(pdp8->pc == 5, "KSF did not ship after flag cleared");
+
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_KCC, "KCC instruction") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8_console_kbd_byte(con, '@');
+
+    pdp8->core[01000] = KSF;        /* this should skip */
+    pdp8->core[01001] = PDP8_OPR_GRP2 | PDP8_OPR_GRP2_HLT;
+    pdp8->core[01002] = KCC;
+    pdp8->core[01003] = KSF;        /* this should NOT skip */
+
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* KSF */
+    
+    pdp8->ac = 01234;
+    pdp8_step(pdp8);        /* KCC */
+
+    ASSERT_V(pdp8->ac == 0, "KCC cleared AC");
+
+    pdp8_step(pdp8);        /* KSF */
+
+    ASSERT_V(pdp8->pc == 01004, "KSF did not skip");
+
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_KCF, "KCF instruction") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8_console_kbd_byte(con, '@');
+
+    pdp8->core[01000] = KSF;        /* this should skip */
+    pdp8->core[01001] = PDP8_OPR_GRP2 | PDP8_OPR_GRP2_HLT;
+    pdp8->core[01002] = KCF;
+    pdp8->core[01003] = KSF;        /* this should NOT skip */
+
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* KSF */
+    
+    pdp8->ac = 01234;
+    pdp8_step(pdp8);        /* KCF */
+
+    ASSERT_V(pdp8->ac == 01234, "KCF did not clear AC");
+
+    pdp8_step(pdp8);        /* KSF */
+
+    ASSERT_V(pdp8->pc == 01004, "KSF did not skip");
+
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_KRS, "KRS instruction") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8_console_kbd_byte(con, '@');
+
+    pdp8->core[01000] = KSF;        /* this should skip */
+    pdp8->core[01001] = PDP8_OPR_GRP2 | PDP8_OPR_GRP2_HLT;
+    pdp8->core[01002] = KRS;
+    pdp8->core[01003] = KSF;        /* this should skip (KRS doesn't clear the flag) */
+
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* KSF */
+    
+    pdp8->ac = 02000;
+    pdp8_step(pdp8);        /* KRS */
+
+    ASSERT_V(pdp8->ac == (02200 | '@'), "KRS or'ed character into AC");
+
+    pdp8_step(pdp8);        /* KSF */
+
+    ASSERT_V(pdp8->pc == 01005, "KSF did skip");
+
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_KIE, "KIE instruction") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8->core[01000] = PDP8_ION;
+    pdp8->core[01001] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0002);
+    pdp8->core[01002] = PDP8_OPR_GRP1 | PDP8_OPR_CLA;
+    pdp8->core[01003] = KIE;    
+    pdp8->core[01004] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0005);
+    pdp8->core[01005] = PDP8_OPR_GRP1 | PDP8_OPR_CLA | PDP8_OPR_GRP1_IAC;
+    pdp8->core[01006] = KIE;    
+    pdp8->core[01007] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0010);
+    
+    pdp8->core[1] = KSF;
+    pdp8->core[2] = PDP8_OPR_GRP2 | PDP8_OPR_GRP2_HLT;
+    pdp8->core[3] = KRB;
+    pdp8->core[4] = KSF;
+    
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* ION */
+    pdp8_step(pdp8);        /* JMP */
+    pdp8_step(pdp8);        /* CLA */
+    pdp8_step(pdp8);        /* KIE */
+
+    /* interrupts are now enabled on the CPU but off on the TTY */
+    pdp8_console_kbd_byte(con, '@');
+
+    pdp8_step(pdp8);        /* JMP */
+
+    ASSERT_V(pdp8->pc == 01005, "did not interrupt with KIE disabled");
+
+    pdp8_step(pdp8);        /* CLA IAC */
+    pdp8_step(pdp8);        /* KIE */
+    pdp8_step(pdp8);        /* KSF */
+
+    ASSERT_V(pdp8->core[0] == 01007, "Interrupt return address stored");
+    ASSERT_V(pdp8->pc == 00003, "Interrupt taken");
+
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_TLF_TSF_TCF, "TFL/TSF/TCF instructions") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+
+    pdp8->core[01000] = PDP8_ION;
+    pdp8->core[01001] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0002);
+    pdp8->core[01002] = TFL;
+    pdp8->core[01003] = PDP8_M_MAKE(PDP8_OP_JMP, PDP8_M_PAGE, 0002);
+        
+    pdp8->core[1] = TSF;
+    pdp8->core[3] = TCF;
+    pdp8->core[4] = TSF;
+    
+    pdp8->pc = 01000;
+
+    pdp8_step(pdp8);        /* ION */
+    pdp8_step(pdp8);        /* JMP */
+    pdp8_step(pdp8);        /* TLF */
+    pdp8_step(pdp8);        /* (interrupt taken) TSF */
+
+    ASSERT_V(pdp8->core[0] == 01003, "interrupt taken");
+    ASSERT_V(pdp8->pc == 00003, "TSF executed in interrupt handler");
+
+    pdp8_step(pdp8);        /* TCF */
+    pdp8_step(pdp8);        /* TSF (should NOT skip) */
+    ASSERT_V(pdp8->pc == 00005, "TSF didn't skip with flag cleared");
+    
+    pdp8_free(pdp8);
+}
+
+DECLARE_TEST(con_TPC_TLS, "TPC/TLS instructions") {
+    pdp8_t *pdp8 = NULL;
+    pdp8_console_t *con = NULL;
+    callback_mock_t callback_mocks;
+
+    int ret = setup(&callback_mocks, &pdp8, &con);
+    ASSERT_V(ret >= 0, "created and installed console");
+
+    if (ret < 0) {
+        return;
+    }
+    
+    pdp8->core[01000] = TPC;
+    pdp8->core[01001] = TSF;
+    pdp8->core[01003] = TCF;
+    pdp8->core[01004] = TLS;
+    pdp8->core[01005] = TSF;
+    
+    pdp8->pc = 01000;
+    pdp8->ac = '@';
+
+    callback_mocks.printed = 0;
+
+    pdp8_step(pdp8);        /* TPC */
+
+    ASSERT_V(callback_mocks.printed == '@', "Character printed");
+
+    pdp8_step(pdp8);        /* TSF */
+    ASSERT_V(pdp8->pc == 01003, "TSF skipped");
+
+    callback_mocks.printed = 0;
+    
+    pdp8_step(pdp8);        /* TCF */
+    pdp8_step(pdp8);        /* TLS */
+
+    ASSERT_V(callback_mocks.printed == '@', "Character printed");
+
+    pdp8_step(pdp8);        /* TSF */
+    ASSERT_V(pdp8->pc == 01007, "TSF skipped");
+    
+    pdp8_free(pdp8);
+}
